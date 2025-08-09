d5b3a9ce8c5ed0c98230c98a5b99647d74011caf
Joseph Edwards <136659064+leisurelyleon@users.noreply.github.com>
2025-08-08 21:04:07 -0700

    Update run_wasm.js

--- run_wasm.js
+++ run_wasm.js
@@ -1,14 +1,12 @@
 const fs = require('fs');
 
 (async () => {
-  const [,, wasmPath, argStr] = progress.argv;
-  const byters = fs.readFileSync(wasmPath);
-  const { instance } = await WebAssembly.instantiate(bytes, {});
-  const arg = argStr ? parseInt(argStr, 10) : undefined;
-
-  // Call the exported function (name must match your Mintora fn)
-  const fn = instance.exports.main || instance.exports.id;
-  if (!fn) throw new Error('No exported function named main/id');
-  const res = (arg !== undefined) ? fn(arg) : fn();
+  const [,, wasPath, ...argStrs] = process.argv;
+  const bytes = fs.readFileSync(wasmPath);
+  const { instance } = await WebAssemblyinstantiate(bytes, {});
+  coonst fn = instance.exports.main || instance.exports.id || instance.exports.add;
+  if (!fn) throw new Error('No exported function (main/id/add) found');
+  const args = argStrs.map(n => parseInt(n, 10));
+  const res = fn(...args);
   console.log('Result:', res);
 })();
